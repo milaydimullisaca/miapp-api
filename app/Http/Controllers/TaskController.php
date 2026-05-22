c2863cb799f7915ce3f01b80c4644c78d195e2eb
milaydimullisca <mileydicondori25@gmail.com>
2026-05-22 09:49:08 -0500

    agregando tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // LISTAR
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    // CREAR
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'titulo' => $request->title,
+            'descripcion' => $request->description,
+            'imagem' => $request->image,
+            'latitud' => $request->latitude,
+            'longitud' => $request->longitude,
+        ]);
+
+        return response()->json($task, 201);
+    }
+
+    // MOSTRAR UNO
+    public function show(string $id)
+    {
+        return response()->json(
+            Task::findOrFail($id)
+        );
+    }
+
+    // EDITAR
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $task->update($request->all());
+
+        return response()->json($task);
+    }
+
+    // ELIMINAR
+    public function destroy(string $id)
+    {
+        Task::destroy($id);
+
+        return response()->json([
+            'message' => 'Task eliminada'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use App\Http\Controllers\PersonaController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-
+Route::apiResource('tasks', TaskController::class);
 /*
 |--------------------------------------------------------------------------
 | Rutas protegidas
