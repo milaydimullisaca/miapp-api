77bbbe99f144dc03c3303068c1be43bcae2e5019
milaydimullisca <mileydicondori25@gmail.com>
2026-06-05 15:43:55 -0500

    cambios

--- database/migrations/2026_06_05_131454_add_niveleducativo_to_tasks_table.php
+++ database/migrations/2026_06_05_131454_add_niveleducativo_to_tasks_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('tasks', function (Blueprint $table) {
-            //
-        });
-    }
+    public function up()
+{
+    Schema::table('tasks', function (Blueprint $table) {
+        $table->string('niveleducativo')->nullable();
+    });
+}
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('tasks', function (Blueprint $table) {
-            //
-        });
-    }
+   public function down()
+{
+    Schema::table('tasks', function (Blueprint $table) {
+        $table->dropColumn('niveleducativo');
+    });
+}
 };
